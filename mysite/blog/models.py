fe5c367a6c30dd49ec0188adeac48226c1a532ed
mmaddy123 <106074170+mmaddy123@users.noreply.github.com>
2022-09-03 14:11:19 +0600

    updating templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,7 @@ from turtle import title
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
