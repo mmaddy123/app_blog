0293ca53425ef954edbdbc0c3ebad17e16fef829
mmaddy123 <106074170+mmaddy123@users.noreply.github.com>
2022-09-10 14:22:41 +0600

    sending email

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mmadem21@gmail.com'
+EMAIL_HOST_PASSWORD = '1377Madmann'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
